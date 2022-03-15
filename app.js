68ff9f9eb91cafeb84a224b63505256718fb20e3
Paulo Valente <paulo.valente@gmail.com>
2022-03-15 11:10:28 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,80 @@
+//jshint esversion:6
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+const encrypt = require("mongoose-encryption");
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema ({
+  email: String,
+  password: String
+});
+
+// encrypt password regardless of any other options. email and _id will be left unencrypted
+userSchema.plugin(encrypt, { secret: process.env.SECRET, requireAuthenticationCode: false, encryptedFields: ["password"] });
+
+
+const User = mongoose.model("User", userSchema);
+const app = express();
+
+console.log();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+	res.render("home");
+});
+
+app.get("/login", function(req, res){
+	res.render("login");
+});
+
+app.get("/register", function(req, res){
+	res.render("register");
+});
+
+app.post("/register", function(req, res){
+
+
+	const newUser = new User ({
+		email: req.body.username,
+		password: req.body.password
+	});
+	newUser.save(function(err){
+		if (err){
+			console.log(err);
+		} else {
+			res.render("secrets");
+		}
+	}
+);
+});
+
+app.post("/login", function(req, res){
+
+	const username = req.body.username;
+	const password = req.body.password;
+
+	User.findOne({name: username}, function(err, foundUser){
+		if (err){
+			console.log(err);
+		} else{
+			if (foundUser){
+				if (foundUser.password===password){
+					res.render("secrets");
+		}}}
+
+	});
+
+
+});
+
+app.listen(3000, function(){
+console.log("Server started on port 3000!");
+});
