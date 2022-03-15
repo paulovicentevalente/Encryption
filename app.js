b7d2c35318aa34c0056840a360c22053e0dafd48
Paulo Valente <paulo.valente@gmail.com>
2022-03-15 19:44:26 -0300

    Implement hash

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
@@ -13,8 +13,7 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-// encrypt password regardless of any other options. email and _id will be left unencrypted
-userSchema.plugin(encrypt, { secret: process.env.SECRET, requireAuthenticationCode: false, encryptedFields: ["password"] });
+
 
 
 const User = mongoose.model("User", userSchema);
@@ -44,7 +43,7 @@ app.post("/register", function(req, res){
 
 	const newUser = new User ({
 		email: req.body.username,
-		password: req.body.password
+		password: md5(req.body.password)
 	});
 	newUser.save(function(err){
 		if (err){
@@ -59,7 +58,7 @@ app.post("/register", function(req, res){
 app.post("/login", function(req, res){
 
 	const username = req.body.username;
-	const password = req.body.password;
+	const password = md5(req.body.password);
 
 	User.findOne({name: username}, function(err, foundUser){
 		if (err){
